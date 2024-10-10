a1da37e11add0297006fc6495050d013fb4d2340
Devansh Kumar <devanshkumar6298@gmail.com>
2024-10-10 22:22:37 +0530

    Frontend completed

---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,37 @@
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            RealEstate simplifies buying, renting, or leasing homes. Search by type, location, and price with advanced filters. Use interactive maps and virtual tours. Get personalized recommendations, read user reviews, and connect with local agents for expert advice. Find your dream house easily with RealEstate. Your perfect home awaits!
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
